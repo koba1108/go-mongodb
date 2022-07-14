b2432aeeeffe06f9e8ffb98e9ee295c70306f191
ykoba <paseli.koba@gmail.com>
2022-07-14 10:42:21 +0000

    init

---
+++ cmd/api/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"log"
+	"github.com/koba1108/go-mongodb/internals/domain/model"
+)
+
+func main() {
+	log.Println("test")
+	model.NewUser("", "")
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/koba1108/go-mongodb
+
+go 1.18
+
+require github.com/ulid/ulid v2.3.0+incompatible // indirect
